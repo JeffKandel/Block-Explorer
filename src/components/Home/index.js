d4fb69a2fc44e6e92f7c1929085df0cf977c8401
Student <jeff.kandel@gmail.com>
2018-01-27 18:59:18 -0500

    feat: add state to Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,15 @@ import Web3 from 'web3'
 let web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          block_ids: [],
+          block_hashes: [],
+          curr_block: null
+        }
+    }
+
     render() {
         console.log('test',web3.eth.accounts);
         return (
