2690a3f4554197047b46538b4b38e345053e85b1
Student <jeff.kandel@gmail.com>
2018-01-27 18:47:19 -0500

    feat: add web3 to Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './style.css';
+import Web3 from 'web3'
+let web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
 
 class Home extends Component {
+    console.log(web3.eth.accounts);
 
     render() {
         return (
